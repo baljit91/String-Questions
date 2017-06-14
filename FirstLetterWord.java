5452d62d7feef832dc522c831587f617623f7258
Baljit Singh <baljitdhanjal@gmail.com>
2017-06-14 19:34:23 -0400

    Create FirstLetterWord.java

--- FirstLetterWord.java
+++ FirstLetterWord.java
@@ -9,6 +9,21 @@ public class FirstLetterWord {
         if(len == 0)
         	return "";
         
+	// crisp logic geeks for geeks	
+	 boolean v = true;
+        //just iterate throug the string
+        for(int i=0; i < len ;i++){
+        	if(s.charAt(i) == ' ')
+        		v = true;
+        	
+        	if(s.charAt(i) != ' ' && v == true){
+        		sb.append(s.charAt(i));
+        		v = false;
+        	}
+        }
+		
+		
+		
         //just iterate throug the string
         for(int i=0; i < len ;i++){
         	sb.append(s.charAt(i));
